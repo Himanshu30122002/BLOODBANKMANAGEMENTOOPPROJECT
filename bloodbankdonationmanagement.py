7327bb35cf80dce280060fae7d2454daede38d43
Himanshu30122002 <83854035+Himanshu30122002@users.noreply.github.com>
2021-09-04 19:12:33 +0530

    Add files via upload

---
+++ bloodbankdonationmanagement.py
@@ -0,0 +1,63 @@
+print("WELCOME TO THE BLOOD BANK MANAGEMENT SYSTEM")
+
+bloodbank = ("sai donor","public services"," new donor","khun ghar")
+
+print("These are the blood banks which are registered under government of india rules and regulations")
+
+print(bloodbank)
+
+print("these are the location where the blood banks situated")
+
+address = ("patna", "lucknow", "meerut", "pune")
+
+
+print("Choose the  blood bank")
+
+name = str(input())
+
+print("enter the location of blood bank ")
+
+location = str(input())
+
+print("for which hospital it is working")
+
+hospital = str(input())
+
+bloodgroup = ("A","B","AB","O")
+
+print(bloodgroup)
+
+print("Enter the blood group you want blood of  ")
+
+blood = input()
+
+print("Thank you,you have choosed the blood group",blood)
+
+print("kindly wait for a minute we will fetch the availability ")
+
+print("yes the blood is available for this blood group ")
+
+print("How much blood do you need ?(please enter in militres)")
+
+demand = int(input())
+
+print("Kindly give your details and purpose")
+
+print("please enter your name")
+
+name  = str(input())
+
+print("enter your aadhar number")
+
+id  = int(input())
+
+print("enter your adress")
+
+ad = str(input())
+
+print("enter your purpose for blood")
+
+reason = str(input())
+
+print("Thank you for your responses")
+
